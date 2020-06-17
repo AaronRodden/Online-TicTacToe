22ecd50fc755b97675bad89cb10ac3a509d8fb67
Aaron Rodden <aaron.rodden21@gmail.com>
2020-06-16 21:48:03 -0700

    Server/client functionallity moved to functions and starting to be integrated with game

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -29,14 +29,217 @@
 int WINDOW_WIDTH = 1000;
 int WINDOW_HEIGHT = 800;
 
+void socketSendMsg(int &iResult, SOCKET socket, const char *sendbuf) {
+	iResult = send(socket, sendbuf, (int)strlen(sendbuf) + 1, 0);
+	if (iResult == SOCKET_ERROR) {
+		printf("send failed: %d\n", WSAGetLastError());
+		closesocket(socket);
+		WSACleanup();
+	}
+
+	printf("Bytes Sent: %ld\n", iResult);
+}
+
+void socketRecieveMsg(int &iResult, SOCKET socket, char recvbuf[], int recvbuflen) {
+	do {
+		iResult = recv(socket, recvbuf, recvbuflen, 0);
+		std::cout << iResult << std::endl;
+		if (iResult > 0) {
+			printf("Bytes received: %d\n", iResult);
+			std::cout << recvbuf << std::endl;
+			break;
+		}
+		else if (iResult == 0)
+			printf("Connection closed\n");
+		else
+			printf("recv failed: %d\n", WSAGetLastError());
+	} while (iResult > 0);
+}
+
+void socketDisconnect(int &iResult, SOCKET socket) {
+	// shutdown the send half of the connection since no more data will be sent
+	std::cout << "Disconnecting..." << std::endl;
+
+	iResult = shutdown(socket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed: %d\n", WSAGetLastError());
+		closesocket(socket);
+		WSACleanup();
+	}
+	// cleanup
+	closesocket(socket);
+	WSACleanup();
+}
+
+SOCKET setupServer() {
+	WSADATA wsaData;
+
+	int iResult;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+	std::cout << "Initalized winsock" << std::endl;
+
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	// Resolve the local address and port to be used by the server
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	std::cout << "Resolved local address and port" << std::endl;
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	// Setup the TCP listening socket
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	std::cout << "Setup TCP listening socket" << std::endl;
+
+	freeaddrinfo(result);
+
+	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+		printf("Listen failed with error: %ld\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET ClientSocket;
+	ClientSocket = INVALID_SOCKET;
+
+	std::cout << "Listening for client socket..." << std::endl;
+
+	// Accept a client socket
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET) {
+		printf("accept failed: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+	std::cout << "Accepted client socket" << std::endl;
+
+	return ClientSocket;
+}
+
+SOCKET connectClient() {
+	int iResult;
+	WSADATA wsaData;
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		std::cout << "WSAStartup failed: " << iResult << std::endl;
+		return 1;
+	}
+
+	struct addrinfo *result = NULL,
+		*ptr = NULL,
+		hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	//int iResult;
+	char const *hardCodeIP = "192.168.2.141";
+
+	// Resolve the server address and port
+	iResult = getaddrinfo(hardCodeIP, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	// Attempt to connect to the first address returned by
+	// the call to getaddrinfo
+	ptr = result;
+
+	// Create a SOCKET for connecting to server
+	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+		ptr->ai_protocol);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	// Connect to server.
+	iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		closesocket(ConnectSocket);
+		ConnectSocket = INVALID_SOCKET;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		WSACleanup();
+		return 1;
+	}
+
+	return ConnectSocket;
+}
+
 // TODO: I think this class is going to become the "Client" class
 // Moves will be made from here then then get sent to the GameState "Server" class
 // GameState then respons and sends information back to all the "clients"
 
 // TODO: Game will recieve the argument for wheter it is client or server
-void game() {
+void game(int playerID) {
 
 	// TODO: Proper code ran here whether you are client or server
+	int iResult;
+	int recvbuflen = DEFAULT_BUFLEN;
+	const char *sendbuf = "this is a test";
+	char recvbuf[DEFAULT_BUFLEN];
+
+	SOCKET playerSocket;
+
+	if (playerID == 0) {
+		SOCKET playerSocket = setupServer();
+	}
+	else {
+		SOCKET playerSocket = connectClient();
+	}
 
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML works!");
 
@@ -47,7 +250,7 @@ void game() {
 	GameState runningState;
 
 	//We may need to make a player class that has all this info
-	int playerID = 0;
+	//int playerID = 0;
 	PieceSpawner spawn(playerID);
 
 	//TODO: Should we even have any of the for loop checks for this given that we are using one at all times?
@@ -152,254 +355,6 @@ void game() {
 	}
 }
 
-int server() {
-	WSADATA wsaData;
-
-	int iResult;
-
-	// Initialize Winsock
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (iResult != 0) {
-		printf("WSAStartup failed: %d\n", iResult);
-		return 1;
-	}
-
-	std::cout << "Initalized winsock" << std::endl;
-
-	struct addrinfo *result = NULL, *ptr = NULL, hints;
-
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
-
-	// Resolve the local address and port to be used by the server
-	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-	if (iResult != 0) {
-		printf("getaddrinfo failed: %d\n", iResult);
-		WSACleanup();
-		return 1;
-	}
-
-	std::cout << "Resolved local address and port" << std::endl;
-
-	SOCKET ListenSocket = INVALID_SOCKET;
-	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-
-	if (ListenSocket == INVALID_SOCKET) {
-		printf("Error at socket(): %ld\n", WSAGetLastError());
-		freeaddrinfo(result);
-		WSACleanup();
-		return 1;
-	}
-
-	// Setup the TCP listening socket
-	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-	if (iResult == SOCKET_ERROR) {
-		printf("bind failed with error: %d\n", WSAGetLastError());
-		freeaddrinfo(result);
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	std::cout << "Setup TCP listening socket" << std::endl;
-
-	freeaddrinfo(result);
-
-	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
-		printf("Listen failed with error: %ld\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	SOCKET ClientSocket;
-	ClientSocket = INVALID_SOCKET;
-
-	std::cout << "Listening for client socket..." << std::endl;
-
-	// Accept a client socket
-	ClientSocket = accept(ListenSocket, NULL, NULL);
-	if (ClientSocket == INVALID_SOCKET) {
-		printf("accept failed: %d\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	std::cout << "Accepted client socket" << std::endl;
-
-	char recvbuf[DEFAULT_BUFLEN];
-	int iSendResult;
-	int recvbuflen = DEFAULT_BUFLEN;
-
-	//TODO: This will be the part that gets called in between game rendering
-	// Just keep all the sockets then pass them into functions that do the
-	// below functionallity. Then the functions return the messages
-
-	// Receive until the peer shuts down the connection
-	do {
-
-		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
-		std::cout << "iResult: " << iResult << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-		if (iResult > 0) {
-			printf("Bytes received: %d\n", iResult);
-			std::cout << recvbuf << std::endl;
-
-			// Echo the buffer back to the sender
-			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
-			if (iSendResult == SOCKET_ERROR) {
-				printf("send failed: %d\n", WSAGetLastError());
-				std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-				closesocket(ClientSocket);
-				WSACleanup();
-				return 1;
-			}
-			printf("Bytes sent: %d\n", iSendResult);
-		}
-		else if (iResult == 0) {
-			printf("Connection closing...\n");
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-		}
-		else {
-			printf("recv failed: %d\n", WSAGetLastError());
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-			closesocket(ClientSocket);
-			WSACleanup();
-			return 1;
-		}
-
-	} while (iResult > 0);
-
-	return 0;
-}
-
-int client() {
-
-	int iResult;
-	WSADATA wsaData;
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (iResult != 0) {
-		std::cout << "WSAStartup failed: " << iResult << std::endl;
-		return 1;
-	}
-
-	struct addrinfo *result = NULL,
-		*ptr = NULL,
-		hints;
-
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-
-	//int iResult;
-	char const *hardCodeIP = "192.168.2.141";
-
-	// Resolve the server address and port
-	iResult = getaddrinfo(hardCodeIP, DEFAULT_PORT, &hints, &result);
-	if (iResult != 0) {
-		printf("getaddrinfo failed: %d\n", iResult);
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-		WSACleanup();
-		return 1;
-	}
-
-	SOCKET ConnectSocket = INVALID_SOCKET;
-	// Attempt to connect to the first address returned by
-	// the call to getaddrinfo
-	ptr = result;
-
-	// Create a SOCKET for connecting to server
-	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
-		ptr->ai_protocol);
-
-	if (ConnectSocket == INVALID_SOCKET) {
-		printf("Error at socket(): %ld\n", WSAGetLastError());
-		freeaddrinfo(result);
-		WSACleanup();
-		return 1;
-	}
-
-	// Connect to server.
-	iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-	if (iResult == SOCKET_ERROR) {
-		closesocket(ConnectSocket);
-		ConnectSocket = INVALID_SOCKET;
-	}
-
-	// Should really try the next address returned by getaddrinfo
-	// if the connect call failed
-	// But for this simple example we just free the resources
-	// returned by getaddrinfo and print an error message
-
-	freeaddrinfo(result);
-
-	if (ConnectSocket == INVALID_SOCKET) {
-		printf("Unable to connect to server!\n");
-		WSACleanup();
-		return 1;
-	}
-
-	int recvbuflen = DEFAULT_BUFLEN;
-
-	char userInput[100];
-	const char *sendbuf = "this is a test";
-
-	std::cin >> userInput;
-
-	char recvbuf[DEFAULT_BUFLEN];
-
-	// Send an initial buffer
-	iResult = send(ConnectSocket, userInput, (int)strlen(sendbuf), 0);
-	if (iResult == SOCKET_ERROR) {
-		printf("send failed: %d\n", WSAGetLastError());
-		closesocket(ConnectSocket);
-		WSACleanup();
-		return 1;
-	}
-
-	printf("Bytes Sent: %ld\n", iResult);
-
-	// shutdown the connection for sending since no more data will be sent
-	// the client can still use the ConnectSocket for receiving data
-	//iResult = shutdown(ConnectSocket, SD_SEND);
-	//if (iResult == SOCKET_ERROR) {
-	//	printf("shutdown failed: %d\n", WSAGetLastError());
-	//	closesocket(ConnectSocket);
-	//	WSACleanup();
-	//	return 1;
-	//}
-
-	// Receive data until the server closes the connection
-	do {
-		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
-		if (iResult > 0) {
-			printf("Bytes received: %d\n", iResult);
-			std::cout << recvbuf << std::endl;
-			std::cin >> userInput;
-			iResult = send(ConnectSocket, userInput, (int)strlen(sendbuf), 0);
-			if (iResult == SOCKET_ERROR) {
-				printf("send failed: %d\n", WSAGetLastError());
-				closesocket(ConnectSocket);
-				WSACleanup();
-				return 1;
-			}
-			printf("Bytes Sent: %ld\n", iResult);
-		}
-		else if (iResult == 0)
-			printf("Connection closed\n");
-		else
-			printf("recv failed: %d\n", WSAGetLastError());
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	} while (iResult > 0);
-
-	return 0;
-}
-
 int main()
 {
 	//game();
@@ -409,10 +364,10 @@ int main()
 	std::cout << "You entered: " << x << std::endl;
 
 	if (x == 0) {
-		server();
+		game(0);
 	}
 	else {
-		client();
+		game(1);
 	}
 	return 0;
 }
